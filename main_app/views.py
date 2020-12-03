e990dcc72b7d752bfce10a5cfb8f3ee0aa8427ea
juliana-zlatevski <juliana.zlatevski@hotmail.com>
2020-12-03 12:19:04 -0600

    adds dynamic profile links to navbar

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from .forms import ProfileForm
+from .forms import ProfileForm, PostForm
 from .models import Profile, Post
 
 # Create your views here.
