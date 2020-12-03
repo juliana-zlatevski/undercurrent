f576976569e579e7ce18e787b1dd971bc3d591bd
juliana-zlatevski <juliana.zlatevski@hotmail.com>
2020-12-02 18:34:20 -0600

    adds profile home page - users are now directed here after they complete account creation

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   # registration/accounts
   path('accounts/signup', views.signup, name='signup'),
   path('profile/new', views.new_profile, name='new_profile'),
+  path('profile/<int:profile_id>/', views.profile_home, name='profile_home'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from .forms import ProfileForm
+from .models import Profile, Post
 
 # Create your views here.
 def home(request):
@@ -28,11 +29,17 @@ def new_profile(request):
           new_profile = form.save(commit=False)
           new_profile.user = request.user
           new_profile.save()
-          return render(request, 'home.html')
-          # return redirect('user_profile', new_profile.id)
+          # return render(request, 'home.html')
+          return redirect('profile_home', new_profile.id)
       else:
           return render(request, 'profile/new.html', {'form': form})
   else: 
         form = ProfileForm()
         context = {'form': form}
         return render(request, 'profile/new.html', context)
+
+def profile_home(request, profile_id):
+    profile = Profile.objects.get(user = request.user)
+    posts = Post.objects.filter(user = request.user)
+    context = {'profile': profile, 'posts': posts}
+    return render(request, 'profile/home.html', context)
