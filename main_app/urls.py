00abea3d6319fb4314888bab8f2ef53b2d5ab0ba
juliana-zlatevski <juliana.zlatevski@hotmail.com>
2020-12-02 21:09:23 -0600

    adds edit functionality to profile model

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
   path('accounts/signup', views.signup, name='signup'),
   path('profile/new', views.new_profile, name='new_profile'),
   path('profile/<int:profile_id>/', views.profile_home, name='profile_home'),
+  path('profile/<int:profile_id>/edit', views.edit_profile, name='edit_profile'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -8,6 +8,7 @@ from .models import Profile, Post
 def home(request):
   return render(request, 'home.html')
 
+# REGISTRATION & PROFILE RELATED VIEWS
 def signup(request):
   error_message = ''
   if request.method == 'POST':
@@ -29,7 +30,6 @@ def new_profile(request):
           new_profile = form.save(commit=False)
           new_profile.user = request.user
           new_profile.save()
-          # return render(request, 'home.html')
           return redirect('profile_home', new_profile.id)
       else:
           return render(request, 'profile/new.html', {'form': form})
@@ -39,7 +39,22 @@ def new_profile(request):
         return render(request, 'profile/new.html', context)
 
 def profile_home(request, profile_id):
-    profile = Profile.objects.get(user = request.user)
-    posts = Post.objects.filter(user = request.user)
-    context = {'profile': profile, 'posts': posts}
-    return render(request, 'profile/home.html', context)
+  profile = Profile.objects.get(user = request.user)
+  posts = Post.objects.filter(user = request.user)
+  context = {'profile': profile, 'posts': posts}
+  return render(request, 'profile/home.html', context)
+
+def edit_profile(request, profile_id):
+  profile = Profile.objects.get(id=profile_id)
+  if request.user == profile.user:
+      if request.method == 'POST':
+          form = ProfileForm(request.POST, instance=profile)
+          if form.is_valid():
+              updated_profile = form.save()
+              return redirect('profile_home', updated_profile.id)
+      else:
+          form = ProfileForm(instance=profile)
+          context = {'form': form}
+          return render(request, 'profile/edit.html', context)
+  else: 
+      raise PermissionDenied("You can't edit another user's profile!")
