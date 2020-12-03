a889b79d175f9b29a81ec480fe6c69ed3f1c37e3
juliana-zlatevski <juliana.zlatevski@hotmail.com>
2020-12-02 21:34:00 -0600

    installed uploadcare - images can be uploaded to profiles, can also be edited successfully

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from .models import *
+from pyuploadcare.dj.forms import ImageField
 
 class ProfileForm(forms.ModelForm):
+    photo = ImageField(label='')
 
     class Meta: 
         model = Profile
-        fields = ['name', 'location', 'bio']
+        fields = ['name', 'location', 'bio', 'photo']
--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
 
 # Create your models here.
 class Post(models.Model):
@@ -16,6 +17,7 @@ class Profile(models.Model):
     join_date = models.DateTimeField(auto_now_add=True)
     bio = models.CharField(max_length=140)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = ImageField(blank=True, manual_crop="")
 
     def __str__(self):
         return self.user.username
